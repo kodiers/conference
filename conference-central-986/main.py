a8f32acd3533c91065a0a64d83f8302ecbdaf255
kodiers <kodiers@gmail.com>
2015-07-02 14:41:28 +0300

    Initial commit

---
+++ conference-central-986/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+from conference import ConferenceApi
+
+from google.appengine.api import app_identity
+from google.appengine.api import mail
+
+class SetAnnouncementHandler(webapp2.RequestHandler):
+    def get(self):
+        """Set Announcement in Memcache."""
+        # TODO 1
+        ConferenceApi._cacheAnnouncement()
+        self.response.set_status(204)
+
+class SendConfirmationEmailHandler(webapp2.RequestHandler):
+    def post(self):
+        """Send email confirming Conference creation."""
+        mail.send_mail(
+            'noreply@%s.appspotmail.com' % (
+                app_identity.get_application_id()),     # from
+            self.request.get('email'),                  # to
+            'You created a new Conference!',            # subj
+            'Hi, you have created a following '         # body
+            'conference:\r\n\r\n%s' % self.request.get(
+                'conferenceInfo')
+        )
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/crons/set_announcement', SetAnnouncementHandler),
+    ('/tasks/send_confirmation_email', SendConfirmationEmailHandler),
+], debug=True)
